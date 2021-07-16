beb5d5bc5cc72492fac085094ed15e4af38bcbba
icosworkouts <icosworkouts@gmail.com>
2021-07-16 23:56:22 +0530

    updated website

---
+++ js/subscribe.js
@@ -0,0 +1,11 @@
+function sendMail(params) {
+    var tempParams = {
+        from_name:document.getElementById("fromName").value,
+        message:document.getElementById("mssg").value,
+    };
+
+    emailjs.send('service_ae3men6','template_fz61bup',tempParams)
+    .then(function(res){
+        console.log("Sucess",res.status);
+    })
+}
--- php/subscribe.php
+++ php/subscribe.php
@@ -64,3 +64,4 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	echo 'ERROR!';
 
 }
+?>
